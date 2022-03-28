f2da64b96bc59e7f6de24423e261d7778deed619
Navid Khan <32226717+Nvd09@users.noreply.github.com>
2022-03-27 21:22:09 -0400

    Update and rename Array Questions to ArrayQuestions.java

--- ArrayQuestions.java
+++ ArrayQuestions.java
@@ -1,7 +1,7 @@
-Question #53
+// Question #53
 
-Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
-A subarray is a contiguous part of an array.
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+// A subarray is a contiguous part of an array.
 
 class Solution {
     public int maxSubArray(int[] nums) {
@@ -20,3 +20,6 @@ class Solution {
         return max;
     }
 }
+
+
+
