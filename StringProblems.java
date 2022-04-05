d64c869da37da244eaf276be1deb56f5e2c86d8e
Navid Khan <32226717+Nvd09@users.noreply.github.com>
2022-04-05 19:06:46 -0400

    Update StringProblems.java

--- StringProblems.java
+++ StringProblems.java
@@ -107,3 +107,46 @@ class Solution {
         return maxLength;
     }
 }
+
+/*
+Question #242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+*/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        Map<Character, Integer> map = new HashMap<>();
+        int length = s.length();
+        for(int i=0; i<length; i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0) +1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i),0) -1);
+        }
+        for(char c : map.keySet()){
+            if(map.get(c) != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        int [] freq = new int[26];
+        for(int i=0; i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        for(int i=0; i<t.length();i++){
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int i: freq){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
